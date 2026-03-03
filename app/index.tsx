556c372dc567f99f1b4f9d05bbc29a16945a1314
Omar Osorio <osorioomar788@gmail.com>
2026-03-03 15:28:16 -0600

    add getPokemon fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    console.log("Entre en pantalla");
+    getPokemon();
+  }, []);
+
+  const getPokemon = async () => {
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Omar Osorio</Text>
     </View>
   );
 }
